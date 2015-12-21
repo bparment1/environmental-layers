9bd9700a7aa1fdfe861ae267a46a068de80e72a6
Benoit Parmentier <benoit.parmentier@gmail.com>
2015-12-21 12:45:00 -0500

    master script stage 4_5 changes to take into account climatology step existence and modifcations in storage

--- climate/research/oregon/interpolation/master_script_stage_4_5.R
+++ climate/research/oregon/interpolation/master_script_stage_4_5.R
@@ -306,17 +306,19 @@ num_cores<-args[10]
 #max number of cells to read in memory
 max_mem<-args[11]
 #rasterOptions(maxmemory=1e+07,timer=TRUE)
+out_path_daily <- file.path(out_path,range_years[1]) #<-c("1992","1993") #right bound not included in the range!!
+out_path_clim <- file.path(out_path,paste("clim_",range_years_clim[1],"_",range_years_clim[2],sep="")) #<-c("1990","2001") #right bound not included in the range!!
 
 #Collect all parameters in a list
 list_param_raster_prediction<-list(list_param_data_prep,screen_data_training,
                                 seed_number,nb_sample,step,constant,prop_minmax,dates_selected,
                                 seed_number_month,nb_sample_month,step_month,constant_month,prop_minmax_month,
-                                list_models,list_models2,interp_method2,lst_avg,out_path,script_path,use_clim_image,join_daily,
+                                list_models,list_models2,interp_method2,lst_avg,out_path_daily,out_path_clim,script_path,use_clim_image,join_daily,
                                 interpolation_method,num_cores,max_mem)
 names(list_param_raster_prediction)<-c("list_param_data_prep","screen_data_training",
                                 "seed_number","nb_sample","step","constant","prop_minmax","dates_selected",
                                 "seed_number_month","nb_sample_month","step_month","constant_month","prop_minmax_month",
-                                "list_models","list_models2","interp_method2","lst_avg","out_path","script_path","use_clim_image","join_daily",
+                                "list_models","list_models2","interp_method2","lst_avg","out_path","out_path_clim","script_path","use_clim_image","join_daily",
                                 "interpolation_method","num_cores","max_mem")
 
 #debug(raster_prediction_fun)
