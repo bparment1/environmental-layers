1cef03c83f70c55ffda2c50c186930cfc2808773
Benoit Parmentier <benoit.parmentier@gmail.com>
2016-06-09 10:45:00 -0400

    mosaicing script master, testing all options for mosaicing

--- climate/research/oregon/interpolation/master_script_stage_7.R
+++ climate/research/oregon/interpolation/master_script_stage_7.R
@@ -14,7 +14,7 @@
 
 #AUTHOR: Benoit Parmentier                                                                        
 #CREATED ON: 01/01/2016  
-#MODIFIED ON: 06/08/2016  
+#MODIFIED ON: 06/09/2016  
 #PROJECT: NCEAS INPLANT: Environment and Organisms                                                                           
 
 #First source these files:
@@ -65,8 +65,8 @@ args<-commandArgs(TRUE)
 
 #script_path <- "/home/parmentier/Data/IPLANT_project/env_layers_scripts"
 script_path <- "/nobackupp8/bparmen1/env_layers_scripts" #path to script
-function_mosaicing_functions <- "global_run_scalingup_mosaicing_function_05312016.R" #PARAM12
-function_mosaicing <-"global_run_scalingup_mosaicing_06082016.R"
+function_mosaicing_functions <- "global_run_scalingup_mosaicing_function_06112016.R" #PARAM12
+function_mosaicing <-"global_run_scalingup_mosaicing_06112016d.R"
 source(file.path(script_path,function_mosaicing)) #source all functions used in this script 
 source(file.path(script_path,function_mosaicing_functions)) #source all functions used in this script 
 
@@ -109,18 +109,20 @@ max_mem = 1e+07 #param 9, arg 9
 mosaicing_method <- "use_edge_weights" #PARAM10, arg 10
 metric_name <- "rmse" # "mae", "r" for MAE, R etc.; can also be ns or nv? #PARAM 11, arg 11
 #metric_name <- "n"
+#metric_name <- "mae"
+
 #day_start <- "19990701" #PARAM 12 arg 12
 #day_end <- "19990703" #PARAM 13 arg 13
-day_start <- "19990108" #PARAM 12 arg 12
+day_start <- "19990107" #PARAM 12 arg 12
 day_end <- "19990108" #PARAM 13 arg 13
 infile_mask <- "/nobackupp8/bparmen1/NEX_data/regions_input_files/r_mask_LST_reg4.tif" #PARAM 14, arg 14
 df_assessment_files_name <- "/nobackupp6/aguzman4/climateLayers/out/reg4/assessment/output_reg4_1999/df_assessment_files_reg4_1999_reg4_1999.txt"  # data.frame with all files used in assessmnet, PARAM 15
 algorithm <- "python" #PARAM 16 #if R use mosaic function for R, if python use modified gdalmerge script from Alberto Guzmann
 #layers_option <- c("var_pred") #arg 17 ,param 17, options are:#res_training, res_testing,ac_training, ac_testing, var_pred
-#layers_option <- c("ac_testing") #arg 17 ,param 17, options are:#res_training, res_testing,ac_training, ac_testing, var_pred
-#layers_option <- c("res_testing") #arg 17 ,param 17, options are:#res_training, res_testing,ac_training, ac_testing, var_pred
-#layers_option <- c("res_training") #arg 17 ,param 17, options are:#res_training, res_testing,ac_training, ac_testing, var_pred
 layers_option <- c("ac_training") #arg 17 ,param 17, options are:#res_training, res_testing,ac_training, ac_testing, var_pred
+# #arg 17 ,param 17, options are:#res_training, res_testing,ac_training, ac_testing, var_pred
+#layers_option <- c("res_training") #arg 17 ,param 17, options are:#res_training, res_testing,ac_training, ac_testing, var_pred
+#layers_option <- c("ac_testing") #arg 17 ,param 17, options are:#res_training, res_testing,ac_training, ac_testing, var_pred
 
 tmp_files <- TRUE #arg 18, param 18, keep temp files if TRUE
 data_type <- "Int16" #, param 19, use int32 for output layers mosaiced
