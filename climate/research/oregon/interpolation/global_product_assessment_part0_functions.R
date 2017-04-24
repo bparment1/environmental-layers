57ba37ccf6fc637b0e8dec0250f83bc1b83c76d7
Benoit Parmentier <benoit.parmentier@gmail.com>
2017-04-24 10:45:00 -0400

    fixing code to allow for TMIN in gap detection

--- climate/research/oregon/interpolation/global_product_assessment_part0_functions.R
+++ climate/research/oregon/interpolation/global_product_assessment_part0_functions.R
@@ -9,7 +9,7 @@
 #
 #AUTHOR: Benoit Parmentier 
 #CREATED ON: 10/31/2016  
-#MODIFIED ON: 12/18/2016            
+#MODIFIED ON: 04/24/2017            
 #Version: 1
 #PROJECT: Environmental Layers project     
 #COMMENTS: removing unused functions and clean up for part0 global prodduct assessment part0 
@@ -497,11 +497,10 @@ predictions_tiles_missing_fun <- function(list_param){
   
   #list_outfiles <- vector("list", length=35) #collect names of output files, this should be dynamic?
   #list_outfiles_names <- vector("list", length=35) #collect names of output files
-
+  #browser(()
+  
   in_dir1_reg <- file.path(in_dir1,region_name)
-  
   list_outfiles <- vector("list", length=14) #collect names of output files
-  
   in_dir_list <- list.dirs(path=in_dir1_reg,recursive=FALSE) #get the list regions processed for this run
 
   #in_dir_list_all  <- unlist(lapply(in_dir_list,function(x){list.dirs(path=x,recursive=F)}))
@@ -537,7 +536,7 @@ predictions_tiles_missing_fun <- function(list_param){
   
   #pred_mod_name <- "mod1"
   list_lf_raster_tif_tiles <- mclapply(in_dir_list_tmp,
-                                    FUN=function(x){list.files(path=x,pattern=paste0("gam_CAI_dailyTmax_predicted_",pred_mod_name,".*.tif"),full.names=T)},
+                                    FUN=function(x){list.files(path=x,pattern=paste0("gam_CAI_",y_var_name,"_predicted_",pred_mod_name,".*.tif"),full.names=T)},
                                     mc.preschedule=FALSE,mc.cores = num_cores)
   list_names_tile_coord <- lapply(list_lf_raster_tif_tiles,FUN=function(x){basename(dirname(dirname(x)))})
   list_names_tile_id <- paste("tile",1:length(list_lf_raster_tif_tiles),sep="_")
@@ -596,15 +595,15 @@ predictions_tiles_missing_fun <- function(list_param){
   #browser()
   
   df_time_series <- test_missing[[1]]$df_time_series
-  head(df_time_series)
+  #head(df_time_series)
 
-  table(df_time_series$missing)
-  table(df_time_series$year)
+  #table(df_time_series$missing)
+  #table(df_time_series$year)
   #browser()
   #####
   #Now combine df_time_series in one table
   
-  dim(test_missing[[1]]$df_time_series)
+  #dim(test_missing[[1]]$df_time_series)
   list_lf <- lapply(1:length(test_missing),FUN=function(i){df_time_series <- as.character(test_missing[[i]]$df_time_series$lf)})
   df_lf_tiles_time_series <- as.data.frame(do.call(cbind,list_lf))
   #http://stackoverflow.com/questions/26220913/replace-na-with-na
@@ -627,6 +626,17 @@ predictions_tiles_missing_fun <- function(list_param){
   filename_df_missing <- file.path(out_dir,paste0("df_missing_by_tiles_predicted_tif_",region_name,"_",pred_mod_name,"_",out_suffix,".txt"))
   write.table(df_missing,file=filename_df_missing)
   
+  #### Generate summary from missing table
+  
+  #1) Select dates with missing tiles
+  #2) Plot number of days missing in maps over 365 days
+  #3) Plot number of days missing in histograms/barplots
+  #4) Keep raster of number pix predictions of overlap
+  
+  df_missing_tiles_day <- subset(df_missing,tot_missing > 0)
+  hist(df_missing$tot_missing)
+  
+  
   ########################
   #### Step 2: examine overlap
   #browser()
@@ -637,7 +647,7 @@ predictions_tiles_missing_fun <- function(list_param){
   
   #collect info: read in all shapefiles
   
-  obj_centroids_shp <- centroids_shp_fun(1,list_shp_reg_files=in_dir_shp_list)
+  #obj_centroids_shp <- centroids_shp_fun(1,list_shp_reg_files=in_dir_shp_list)
                                          
   obj_centroids_shp <- mclapply(1:length(in_dir_shp_list),
                                 FUN=centroids_shp_fun,
@@ -683,29 +693,35 @@ predictions_tiles_missing_fun <- function(list_param){
   tile_coord <- basename(in_dir_reg[1])
   date_val <- df_missing$date[1]
 
-  ### use rasterize
-  #spdf_tiles <- do.call(bind, shps_tiles) #bind all tiles together in one shapefile
-  #Error in (function (classes, fdef, mtable)  : 
-  #unable to find an inherited method for function 'bind' for signature '"missing", "missing"'
-
-  #undebug(rasterize_tile_day)
-  #list_predicted <- rasterize_tile_day(1,
-  #        list_spdf=shps_tiles,
-  #         df_missing=df_missing,
-  #         list_r_ref=list_r_ref,
-  #         col_name="overlap",
-  #         date_val=df_missing$date[1])
-  #list_predicted <- mclapply(1:6,
-  #         FUN=rasterize_tile_day,
-  #         list_spdf=shps_tiles,
-  #         df_missing=df_missing,
-  #         list_r_ref=list_r_ref,
-  #         col_name = "overlap",
-  #         date_val=df_missing$date[1],
-  #          mc.preschedule=FALSE,
-  #         mc.cores = num_cores)
+  if(raster_map==TRUE){
+    
+    #d
+    #d
+    
+  }
+    ### use rasterize
+    #spdf_tiles <- do.call(bind, shps_tiles) #bind all tiles together in one shapefile
+    #Error in (function (classes, fdef, mtable)  : 
+    #unable to find an inherited method for function 'bind' for signature '"missing", "missing"'
   
-  list_predicted <- mclapply(1:length(shps_tiles),
+    #undebug(rasterize_tile_day)
+    #list_predicted <- rasterize_tile_day(1,
+    #        list_spdf=shps_tiles,
+    #         df_missing=df_missing,
+    #         list_r_ref=list_r_ref,
+    #         col_name="overlap",
+    #         date_val=df_missing$date[1])
+    #list_predicted <- mclapply(1:6,
+    #         FUN=rasterize_tile_day,
+    #         list_spdf=shps_tiles,
+    #         df_missing=df_missing,
+    #         list_r_ref=list_r_ref,
+    #         col_name = "overlap",
+    #         date_val=df_missing$date[1],
+    #          mc.preschedule=FALSE,
+    #         mc.cores = num_cores)
+  
+    list_predicted <- mclapply(1:length(shps_tiles),
            FUN=rasterize_tile_day,
            list_spdf=shps_tiles,
            df_missing=df_missing,
@@ -718,46 +734,46 @@ predictions_tiles_missing_fun <- function(list_param){
             mc.preschedule=FALSE,
            mc.cores = num_cores)
 
-  ##check that everything is correct:
-  #plot(r_mask)
-  #plot(raster(list_predicted[[1]]),add=T)
-  #plot(spdf_tiles_test,add=T,border="green",usePolypath = FALSE) #added usePolypath following error on brige and NEX
+    ##check that everything is correct:
+    #plot(r_mask)
+    #plot(raster(list_predicted[[1]]),add=T)
+    #plot(spdf_tiles_test,add=T,border="green",usePolypath = FALSE) #added usePolypath following error on brige and NEX
 
-  ### Make a list of file
-  out_suffix_str_tmp <- paste0(region_name,"_",out_suffix,"_tmp")
-  out_dir_str <- out_dir
-  filename_list_predicted <- file.path(out_dir_str,paste("list_to_mosaics_",out_suffix_str_tmp,".txt",sep=""))
+    ### Make a list of file
+    out_suffix_str_tmp <- paste0(region_name,"_",out_suffix,"_tmp")
+    out_dir_str <- out_dir
+    filename_list_predicted <- file.path(out_dir_str,paste("list_to_mosaics_",out_suffix_str_tmp,".txt",sep=""))
 
-  #writeLines(unlist(list_weights_m),con=filename_list_mosaics_weights_m) #weights files to mosaic 
-  #writeLines(unlist(list_weights_prod_m),con=filename_list_mosaics_prod_weights_m) #prod weights files to mosaic
+    #writeLines(unlist(list_weights_m),con=filename_list_mosaics_weights_m) #weights files to mosaic 
+    #writeLines(unlist(list_weights_prod_m),con=filename_list_mosaics_prod_weights_m) #prod weights files to mosaic
       
-  writeLines(unlist(list_predicted),con=filename_list_predicted) #weights files to mosaic 
+    writeLines(unlist(list_predicted),con=filename_list_predicted) #weights files to mosaic 
 
-  #browser()
+    #browser()
   
-  #out_mosaic_name_weights_m <- r_weights_sum_raster_name <- file.path(out_dir,paste("r_weights_sum_m_",mosaic_method,"_weighted_mean_",out_suffix,".tif",sep=""))
-  #out_mosaic_name_prod_weights_m <- r_weights_sum_raster_name <- file.path(out_dir,paste("r_prod_weights_sum_m_",mosaic_method,"_weighted_mean_",out_suffix,".tif",sep=""))
-  out_mosaic_name_predicted_m  <- file.path(out_dir_str,paste("r_overlap_sum_m_",out_suffix_str_tmp,"_tmp",".tif",sep=""))
+    #out_mosaic_name_weights_m <- r_weights_sum_raster_name <- file.path(out_dir,paste("r_weights_sum_m_",mosaic_method,"_weighted_mean_",out_suffix,".tif",sep=""))
+    #out_mosaic_name_prod_weights_m <- r_weights_sum_raster_name <- file.path(out_dir,paste("r_prod_weights_sum_m_",mosaic_method,"_weighted_mean_",out_suffix,".tif",sep=""))
+    out_mosaic_name_predicted_m  <- file.path(out_dir_str,paste("r_overlap_sum_m_",out_suffix_str_tmp,"_tmp",".tif",sep=""))
 
-  rast_ref_name <- infile_mask
-  mosaic_python <- "/nobackupp6/aguzman4/climateLayers/sharedCode/"
-  rast_ref_name <- infile_mask
-  #python /nobackupp6/aguzman4/climateLayers/sharedCode//gdal_merge_sum.py --config GDAL_CACHEMAX=1500 --overwrite=TRUE -o /nobackupp8/bparmen1/climateLayers/out
-  mosaic_overlap_tiles_obj <- mosaic_python_merge(NA_flag_val=NA_flag_val,
+    rast_ref_name <- infile_mask
+    mosaic_python <- "/nobackupp6/aguzman4/climateLayers/sharedCode/"
+    rast_ref_name <- infile_mask
+    #python /nobackupp6/aguzman4/climateLayers/sharedCode//gdal_merge_sum.py --config GDAL_CACHEMAX=1500 --overwrite=TRUE -o /nobackupp8/bparmen1/climateLayers/out
+    mosaic_overlap_tiles_obj <- mosaic_python_merge(NA_flag_val=NA_flag_val,
                                                 module_path=mosaic_python,
                                                 module_name="gdal_merge_sum.py",
                                                 input_file=filename_list_predicted,
                                                 out_mosaic_name=out_mosaic_name_predicted_m,
                                                 raster_ref_name = rast_ref_name) ##if NA, not take into account
-  r_overlap_raster_name <- mosaic_overlap_tiles_obj$out_mosaic_name
-  cmd_str1 <-   mosaic_overlap_tiles_obj$cmd_str
+    r_overlap_raster_name <- mosaic_overlap_tiles_obj$out_mosaic_name
+    cmd_str1 <-   mosaic_overlap_tiles_obj$cmd_str
 
-  r_overlap <- raster(r_overlap_raster_name)
-  r_mask <- raster(infile_mask)
+    r_overlap <- raster(r_overlap_raster_name)
+    r_mask <- raster(infile_mask)
   
-  out_mosaic_name_overlap_masked  <- file.path(out_dir_str,paste("r_overlap_sum_masked_",region_name,"_",out_suffix,".tif",sep=""))
+    out_mosaic_name_overlap_masked  <- file.path(out_dir_str,paste("r_overlap_sum_masked_",region_name,"_",out_suffix,".tif",sep=""))
 
-  r_overlap_m <- mask(r_overlap,
+    r_overlap_m <- mask(r_overlap,
                   mask=r_mask,
                   filename=out_mosaic_name_overlap_masked,
                   datatype=data_type_str,
@@ -766,64 +782,64 @@ predictions_tiles_missing_fun <- function(list_param){
                   overwrite=TRUE,
                   NAflag=NA_flag_val)
 
-  #r_overlap_m <- mask(r_overlap,r_mask,filename=out_mosaic_name_overlap_masked,overwrite=T)
-  #plot(r_overlap_m)
-  #plot(spdf_tiles_test,add=T,border="green",usePolypath = FALSE) #added usePolypath following error on brige and NEX
+    #r_overlap_m <- mask(r_overlap,r_mask,filename=out_mosaic_name_overlap_masked,overwrite=T)
+    #plot(r_overlap_m)
+    #plot(spdf_tiles_test,add=T,border="green",usePolypath = FALSE) #added usePolypath following error on brige and NEX
   
-  #r_table <- ratify(r_overlap_m) # build the Raster Attibute table
-  #rat <- levels(r_table)[[1]]#get the values of the unique cell frot the attribute table
-  #rat$legend <- paste0("tile_",1:26)
-  tb_freq_overlap <- as.data.frame(freq(r_overlap_m))
-  #rat$legend <- tb_freq$value
-  #levels(r_table) <- rat
+    #r_table <- ratify(r_overlap_m) # build the Raster Attibute table
+    #rat <- levels(r_table)[[1]]#get the values of the unique cell frot the attribute table
+    #rat$legend <- paste0("tile_",1:26)
+    tb_freq_overlap <- as.data.frame(freq(r_overlap_m))
+    #rat$legend <- tb_freq$value
+    #levels(r_table) <- rat
   
-  res_pix <- 800
-  #res_pix <- 480
-  col_mfrow <- 1
-  row_mfrow <- 1
+    res_pix <- 800
+    #res_pix <- 480
+    col_mfrow <- 1
+    row_mfrow <- 1
   
-  png_filename_maximum_overlap <-  file.path(out_dir,paste("Figure_maximum_overlap_",region_name,"_",out_suffix,".png",sep =""))
+    png_filename_maximum_overlap <-  file.path(out_dir,paste("Figure_maximum_overlap_",region_name,"_",out_suffix,".png",sep =""))
     
-  title_str <-  paste("Maximum overlap: Number of predicted pixels for ",variable_name, sep = "")
+    title_str <-  paste("Maximum overlap: Number of predicted pixels for ",variable_name, sep = "")
   
-  png(filename=png_filename_maximum_overlap,width = col_mfrow * res_pix,height = row_mfrow * res_pix)
+    png(filename=png_filename_maximum_overlap,width = col_mfrow * res_pix,height = row_mfrow * res_pix)
     #my_col=c('blue','red','green')
-  my_col <- rainbow(length(tb_freq_overlap$value))
+    my_col <- rainbow(length(tb_freq_overlap$value))
 
-  plot(r_overlap_m,col=my_col,legend=F,box=F,axes=F,main=title_str)
-  legend(x='topright', legend =tb_freq_overlap$value,fill = my_col,cex=0.8)
+    plot(r_overlap_m,col=my_col,legend=F,box=F,axes=F,main=title_str)
+    legend(x='topright', legend =tb_freq_overlap$value,fill = my_col,cex=0.8)
   
-  dev.off()
+    dev.off()
 
-  ### now assign id and match extent for tiles
+    ### now assign id and match extent for tiles
   
-  lf_files <- unlist(list_predicted)
-  rast_ref_name <- infile_mask
-  rast_ref <- rast_ref_name
+    lf_files <- unlist(list_predicted)
+    rast_ref_name <- infile_mask
+    rast_ref <- rast_ref_name
   
-  ##Maching resolution is probably only necessary for the r mosaic function
-  #Modify later to take into account option R or python...
-  list_param_raster_match <- list(lf_files,rast_ref,file_format,python_bin,out_suffix_str_tmp,out_dir_str)
-  names(list_param_raster_match) <- c("lf_files","rast_ref","file_format","python_bin","out_suffix","out_dir_str")
+    ##Maching resolution is probably only necessary for the r mosaic function
+    #Modify later to take into account option R or python...
+    list_param_raster_match <- list(lf_files,rast_ref,file_format,python_bin,out_suffix_str_tmp,out_dir_str)
+    names(list_param_raster_match) <- c("lf_files","rast_ref","file_format","python_bin","out_suffix","out_dir_str")
 
-  #undebug(raster_match)
-  #r_test <- raster_match(1,list_param_raster_match)
-  #r_test <- raster(raster_match(1,list_param_raster_match))
+    #undebug(raster_match)
+    #r_test <- raster_match(1,list_param_raster_match)
+    #r_test <- raster(raster_match(1,list_param_raster_match))
 
-  list_tiles_predicted_m <- unlist(mclapply(1:length(lf_files),
+    list_tiles_predicted_m <- unlist(mclapply(1:length(lf_files),
                                             FUN=raster_match,list_param=list_param_raster_match,
                                             mc.preschedule=FALSE,mc.cores = num_cores))                           
 
-  extension_str <- extension(lf_files)
-  raster_name_tmp <- gsub(extension_str,"",basename(lf_files))
-  out_suffix_str <- paste0(region_name,"_",out_suffix)
-  raster_name <- file.path(out_dir_str,paste(raster_name_tmp,"_","masked_",out_suffix_str,file_format,sep=""))
+    extension_str <- extension(lf_files)
+    raster_name_tmp <- gsub(extension_str,"",basename(lf_files))
+    out_suffix_str <- paste0(region_name,"_",out_suffix)
+    raster_name <- file.path(out_dir_str,paste(raster_name_tmp,"_","masked_",out_suffix_str,file_format,sep=""))
   
-  #writeRaster(r, NAflag=NA_flag_val,filename=raster_name,overwrite=TRUE)  
+    #writeRaster(r, NAflag=NA_flag_val,filename=raster_name,overwrite=TRUE)  
 
-  #r_stack <- stack(list_tiles_predicted_m)
-  list_mask_out_file_name <- raster_name
-  list_tiles_predicted_masked <- unlist(mclapply(1:length(list_tiles_predicted_m),
+    #r_stack <- stack(list_tiles_predicted_m)
+    list_mask_out_file_name <- raster_name
+    list_tiles_predicted_masked <- unlist(mclapply(1:length(list_tiles_predicted_m),
                                                  FUN=function(i){mask(raster(list_tiles_predicted_m[i]),
                                                                       r_mask,filename=list_mask_out_file_name[i],
                                                                       overwrite=T,
@@ -831,24 +847,24 @@ predictions_tiles_missing_fun <- function(list_param){
                                                                       options=c("COMPRESS=LZW"))},
                                                  mc.preschedule=FALSE,
                                                  mc.cores = num_cores))                         
-  #r_stack_masked <- mask(r, m2) #, maskvalue=TRUE)
+    #r_stack_masked <- mask(r, m2) #, maskvalue=TRUE)
 
-  ########################
-  #### Step 3: combine overlap information and number of predictions by day
-  ##Now loop through every day if missing then generate are raster showing map of number of prediction
+    ########################
+    #### Step 3: combine overlap information and number of predictions by day
+    ##Now loop through every day if missing then generate are raster showing map of number of prediction
    
-  #browser()
-  #Debugged on 12/16
-  #r_tiles_stack <- stack(list_tiles_predicted_masked)
-  #names(r_tiles_stack) <- basename(in_dir_reg) #this does not work, X. is added to the name, use list instead
+    #browser()
+    #Debugged on 12/16
+    #r_tiles_stack <- stack(list_tiles_predicted_masked)
+    #names(r_tiles_stack) <- basename(in_dir_reg) #this does not work, X. is added to the name, use list instead
   
-  names(list_tiles_predicted_masked) <- basename(in_dir_reg)
-  df_missing_tiles_day <- subset(df_missing,tot_missing > 0)
-  #r_tiles_s <- r_tiles_stack
-  names_tiles <- basename(in_dir_reg)
+    names(list_tiles_predicted_masked) <- basename(in_dir_reg)
+    df_missing_tiles_day <- subset(df_missing,tot_missing > 0)
+    #r_tiles_s <- r_tiles_stack
+    names_tiles <- basename(in_dir_reg)
   
 
-  list_param_generate_raster_number_pred <- list(list_tiles_predicted_masked,df_missing_tiles_day,r_overlap_m,
+    list_param_generate_raster_number_pred <- list(list_tiles_predicted_masked,df_missing_tiles_day,r_overlap_m,
                                                  num_cores,region_name,data_type,scaling,python_bin,
                                                  plotting_figures,
                                                  NA_flag_val,out_suffix,out_dir)
