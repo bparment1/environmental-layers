b21bb8718466da350c176005b1d2b784f8b7d682
bparment1 <benoit.parmentier@gmail.com>
2017-06-14 10:45:00 -0400

    generating figures from assembled gap detection data

--- climate/research/oregon/interpolation/gap_detection_product_assessment.R
+++ climate/research/oregon/interpolation/gap_detection_product_assessment.R
@@ -9,7 +9,7 @@
 #
 #AUTHOR: Benoit Parmentier 
 #CREATED ON: 10/31/2016  
-#MODIFIED ON: 06/07/2017            
+#MODIFIED ON: 06/14/2017            
 #Version: 1
 #PROJECT: Environmental Layers project     
 #COMMENTS: removing unused functions and clean up for part0 global prodduct assessment part0 
@@ -69,7 +69,7 @@ library(lubridate)
 var <- "TMIN" # variable being interpolated #PARAM 1, arg 1
 in_dir <- "/nobackupp6/aguzman4/climateLayers/tMinOut/reg1/assessment" #PARAM2
 region_name <- c("reg1") #PARAM 3, arg 3
-out_suffix <- "predictions_gaps_tiles_assessment_reg1_1993" #PARAM 4
+out_suffix <- "mosaic_gaps_tiles_assessment_reg1_combined" #PARAM 4
 #out_suffix_str <- region_name #PARAM 4, CONST 3
 out_dir <- "/nobackupp8/bparmen1/climateLayers/tMinOut/reg1/assessment"
 create_out_dir_param <- TRUE #PARAM 12, arg 6
@@ -94,134 +94,184 @@ in_dir <- "/nobackupp6/aguzman4/climateLayers/tMinOut/testGaps"
 # ./*reg1*/df_missing_by_dates_tiles_predicted_tif_reg1_mod1_predictions_gaps_tiles_assessment_reg1_*.txt 
 num_cores <- 6
 pattern_str <- "df_missing_by_dates_tiles_predicted_tif_reg1_mod1_predictions_gaps_tiles_assessment_reg1_.*.txt"
+countries_shp <- "/nobackupp8/bparmen1/NEX_data/countries.shp" #PARAM 17
+infile_mask <- "/nobackupp8/bparmen1/NEX_data/regions_input_files/r_mask_LST_reg1.tif" #PARAM 14, arg 14
+
+var <- "TMIN" # variable being interpolated #PARAM 1, arg 1
+in_dir <- "/nobackupp6/aguzman4/climateLayers/tMinOut/reg1/assessment" #PARAM2
+region_name <- c("reg1") #PARAM 3, arg 3
+out_suffix <- "mosaic_gaps_tiles_assessment_reg1_combined" #PARAM 4
+#out_suffix_str <- region_name #PARAM 4, CONST 3
+out_dir <- "/nobackupp8/bparmen1/climateLayers/tMinOut/reg1/assessment"
+create_out_dir_param <- TRUE #PARAM 12, arg 6
+
+### constant
+
+pred_mod_name <- "mod1"
+#pred_mod_name <- "mod1"
+date_start <- day_start
+date_end <- day_end
+NA_value <- -32768 #PARAM 26
+NA_flag_val <- NA_value #PARAM 26
+proj_str <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +towgs84=0,0,0"
+interpolation_method<-c("gam_CAI") #PARAM 2
+day_to_mosaic_range <- NULL #PARAM 7
+file_format <- ".tif" #format for mosaiced files # PARAM 14
+#python_bin <- "/usr/bin" #PARAM 15, NCEAS
+python_bin <- "/nobackupp6/aguzman4/climateLayers/sharedModules2/bin" #PARAM 15"
+in_dir_list_filename <- NULL # PARAM 16, if NULL, use the in_dir directory to search for info
+#countries_shp <-"/data/project/layers/commons/NEX_data/countries.shp" #Atlas
+countries_shp <- "/nobackupp8/bparmen1/NEX_data/countries.shp" #PARAM 17
+lf_raster <- NULL #list of raster to consider #PARAM 18
+scaling <- 1
+data_type <- "Int16"
+out_suffix_str <- region_name #PARAM 4, CONST 3
+
+##Add for precip later...
+if (var == "TMAX") {
+  y_var_name <- "dailyTmax"
+  y_var_month <- "TMax"
+}
+if (var == "TMIN") {
+  y_var_name <- "dailyTmin"
+  y_var_month <- "TMin"
+}
+
+##Add for precip later...
+if (var == "TMAX") {
+  variable_name <- "maximum temperature"
+}
+if (var == "TMIN") {
+  variable_name <- "minimum temperature"
+}
 
 function(in_dir,region_name,num_cores,out_dir, out_suffix){
   #d
   #d
+  ## select relevant files for region
+  in_dir_list_tmp <- list.files(pattern =paste0(".*.",region_name,".*."),full.names=T,path=in_dir)
+  
+  list_lf_df_missing_tiles <- unlist(mclapply(in_dir_list_tmp,
+                                              FUN=function(x){list.files(path=x,
+                                                                         pattern=paste0("df_missing_by_dates_tiles_predicted_tif_",".*.",region_name,".*.txt"),full.names=T)},
+                                              mc.preschedule=FALSE,mc.cores = num_cores))
+  list_df_missing_tiles <- mclapply(list_lf_df_missing_tiles,
+                                    FUN=function(x){read.table(x,sep=",",stringsAsFactors = F,check.names = F)},
+                                    mc.preschedule=FALSE,mc.cores = num_cores)
+  
+  df_missing_tiles_reg <- do.call(rbind,list_df_missing_tiles)
+
+
+  ###### Assessment ####
+  #### Generate summary from missing table
+  #1) Select dates with missing tiles
+  #2) Plot number of days missing in maps over 365 days
+  #3) Plot number of days missing in histograms/barplots
+  #4) Keep raster of number pix predictions of overlap
+  ### do sum across tiles to find number of missing per tiles and map it
+  
+  n_tiles <- length(df_missing_tiles_reg) - 3
+  df_missing_tiles_reg_sp <- t(df_missing_tiles_reg[,1:n_tiles]) #transpose to get df with lines centroids of tiles
+  df_missing_tiles_reg_sp <- as.data.frame(df_missing_tiles_reg_sp)
+  names(df_missing_tiles_reg_sp) <- df_missing_tiles_reg$date
+  df_missing_tiles_reg_sp$tot_missing <- rowSums(df_missing_tiles_reg_sp) #total missing over a year by tile
+  df_missing_tiles_reg_sp$tot_pred <- length(df_missing_tiles_reg$date) - df_missing_tiles_reg_sp$tot_missing
+  
+  tiles_names <- names(df_missing_tiles_reg)[1:n_tiles]
+  list_xy <- strsplit(unlist(basename(tiles_names)),"_")
+
+  #df_missing_tiles_reg_sp <- df_missing_tiles_reg
+  #list_xy <- lapply(centroids_pts,function(x){coordinates(x)})
+  coord_xy <- do.call(rbind,list_xy)
+  y_val <- as.numeric(coord_xy[,1]) #lat, long! need to reverse
+  x_val <- as.numeric(coord_xy[,2])
+  coordinates(df_missing_tiles_reg_sp) <- as.matrix(cbind(x_val,y_val))
+  
+  #This contains in rows date and tiles columns
+  filename_df_missing_tiles_sp <- file.path(out_dir,paste0("df_missing_by_centroids_tiles_and_dates",region_name,"_",pred_mod_name,"_",out_suffix,".txt"))
+  write.table(as.data.frame(df_missing_tiles_sp),file=filename_df_missing_tiles_sp,sep=",")
+
+  #browser()
+  
+  ### Now generate plots if missing tiles for specific dates in the region
+  df_missing_tiles_day <- subset(df_missing_tiles_reg,tot_missing > 0)
+  
+  path_to_shp <- dirname(countries_shp)
+  layer_name <- sub(".shp","",basename(countries_shp))
+  reg_layer <- readOGR(path_to_shp, layer_name) #outlines of the region
+  r_mask <- raster(infile_mask)
+  
+  ##### write basic stats in textfile?
+  #sum(df_missing_tiles_reg)
+  range(df_missing_tiles_reg$tot_missing)
+  #sink()
+  missing_val <- table(df_missing_tiles_reg$tot_missing) #save this info!!
+   
+  if(nrow(df_missing_tiles_day)>0){
+    
+    res_pix <- 800
+    #res_pix <- 480
+    col_mfrow <- 1
+    row_mfrow <- 1
+    png_filename_histogram <-  file.path(out_dir,paste("Figure_barplot_","region_missing_tiles","_",out_suffix,".png",sep =""))
+    
+    png(filename=png_filename_histogram,width = col_mfrow * res_pix,height = row_mfrow * res_pix)
+    barplot(missing_val,
+            ylab="frequency of missing",
+            xlab="number of missing day predictions",
+            main=paste0("Number of missing predictions over 31 years for ",region_name))
+    
+    #check for this in every output, if not present then there are no missing tiles over the full year for 
+    #the specific region
+    write.table(df_missing_tiles_day,file=paste0("df_missing_tiles_day_mosaic_",out_suffix,".txt"))
+    
+    ### Now output the table:
+    out_filename <- paste0("missing_val_table_barplot_day_tiles_",out_suffix,".txt")
+    write.table(missing_val)
+    #do spplot after that on tot sum
+    
+    png(filename=paste("Figure_total_missing_days_map_centroids_tile_",pred_mod_name,"_",
+                       "_",out_suffix,".png",sep=""),
+        width=col_mfrow*res_pix,height=row_mfrow*res_pix)
+    #spplot for reg_layer not working on NEX again
+    #p_shp <- spplot(reg_layer,"ISO3" ,col.regions=NA, col="black") #ok problem solved!!
+    p_r <-levelplot(r_mask,colorkey=F) #no key legend
+    p <- bubble(df_missing_tiles_reg_sp,"tot_missing",main=paste0("Missing per tile and by ",pred_mod_name,
+                                                              " for ",y_var_name))
+    #p1 <- p+p_shp
+    p_c <- p + p_r + p #set the legend first by using p first
+    
+    try(print(p_c)) #error raised if number of missing values below a threshold does not exist
+    dev.off()
+    
+  }else{
+    #do spplot after that on tot sum
+    res_pix <- 800
+    #res_pix <- 480
+    col_mfrow <- 1
+    row_mfrow <- 1
+    
+    png(filename=paste("Figure_total_predicted_days_predicted_map_centroids_tile_",pred_mod_name,"_",
+                       "_",out_suffix,".png",sep=""),
+        width=col_mfrow*res_pix,height=row_mfrow*res_pix)
+    
+    p_r <-levelplot(r_mask,colorkey=F) #no key legend
+    p <- bubble(df_missing_tiles_sp,"tot_pred",main=paste0("Prediction per tile and by ",pred_mod_name,
+                                                           " for ", y_var_name))
+    p_c <- p + p_r + p #set the legend first by using p first
+    #p1 <- p+p_shp
+    try(print(p_c)) #error raised if number of missing values below a threshold does not exist
+    dev.off()
+  }
+  
+  #### add more here
   
 }
-## select relevant files for region
-in_dir_list_tmp <- list.files(pattern =paste0(".*.",region_name,".*."),full.names=T,path=in_dir)
-
-list_lf_df_missing_tiles <- unlist(mclapply(in_dir_list_tmp,
-                                     FUN=function(x){list.files(path=x,
-                                                                pattern=paste0("df_missing_by_dates_tiles_predicted_tif_",".*.",region_name,".*.txt"),full.names=T)},
-                                     mc.preschedule=FALSE,mc.cores = num_cores))
-list_df_missing_tiles <- mclapply(list_lf_df_missing_tiles,
-                                     FUN=function(x){read.table(x,sep=",",stringsAsFactors = F,check.names = F)},
-                                     mc.preschedule=FALSE,mc.cores = num_cores)
-
-df_missing_tiles_reg <- do.call(rbind,list_df_missing_tiles)
-sum(df_missing_tiles_reg)
-
-range(df_missing_tiles_reg$tot_missing)
-table(df_missing_tiles_reg$tot_missing)
-histogram(df_missing_tiles_reg$tot_missing)
 
 
-###### Assessment ####
-#### Generate summary from missing table
-#1) Select dates with missing tiles
-#2) Plot number of days missing in maps over 365 days
-#3) Plot number of days missing in histograms/barplots
-#4) Keep raster of number pix predictions of overlap
-### do sum across tiles to find number of missing per tiles and map it
 
-n_tiles <- length(df_missing_tiles_reg) - 3
-df_missing_tiles_reg_sp <- t(df_missing_tiles_reg[,1:n_tiles]) #transpose to get df with lines centroids of tiles
-df_missing_tiles_reg_sp <- as.data.frame(df_missing_tiles_reg_sp)
-names(df_missing_tiles_reg_sp) <- df_missing_tiles_reg$date
-df_missing_tiles_reg_sp$tot_missing <- rowSums(df_missing_tiles_reg_sp) #total missing over a year by tile
-df_missing_tiles_reg_sp$tot_pred <- length(df_missing_tiles_reg$date) - df_missing_tiles_reg_sp$tot_missing
-
-tiles_names <- names(df_missing_tiles_reg)[1:n_tiles]
-list_xy <- strsplit(unlist(basename(tiles_names)),"_")
-sub("X","",list_xy)
-
-#list_xy <- lapply(centroids_pts,function(x){coordinates(x)})
-coord_xy <- do.call(rbind,list_xy)
-y_val <- as.numeric(coord_xy[,1]) #lat, long! need to reverse
-x_val <- as.numeric(coord_xy[,2])
-coordinates(df_missing_tiles_sp) <- as.matrix(cbind(x_val,y_val))
-
-#This contains in rows date and tiles columns
-filename_df_missing_tiles_sp <- file.path(out_dir,paste0("df_missing_by_centroids_tiles_and_dates",region_name,"_",pred_mod_name,"_",out_suffix,".txt"))
-write.table(as.data.frame(df_missing_tiles_sp),file=filename_df_missing_tiles_sp,sep=",")
-
-#browser()
-
-### Now generate plots if missing tiles for specific dates in the region
-df_missing_tiles_day <- subset(df_missing,tot_missing > 0)
-path_to_shp <- dirname(countries_shp)
-layer_name <- sub(".shp","",basename(countries_shp))
-reg_layer <- readOGR(path_to_shp, layer_name) #outlines of the region
-r_mask <- raster(infile_mask)
-
-if(nrow(df_missing_tiles_day)>0){
-  
-  res_pix <- 800
-  #res_pix <- 480
-  col_mfrow <- 1
-  row_mfrow <- 1
-  png_filename_histogram <-  file.path(out_dir,paste("Figure_histogram_","region_missing_tiles","_",out_suffix,".png",sep =""))
-  
-  png(filename=png_filename_histogram,width = col_mfrow * res_pix,height = row_mfrow * res_pix)
-  hist(df_missing$tot_missing,
-       ylab="frequency of missing",
-       xlab="tiles",
-       main="Number of missing predictions over a year by tile")
-  dev.off()
-  
-  #check for this in every output, if not present then there are no missing tiles over the full year for 
-  #the specific region
-  write.table(df_missing_tiles_day,file=paste0("df_missing_tiles_day_mosaic_",out_suffix,".txt"))
-  
-  #do spplot after that on tot sum
-  
-  png(filename=paste("Figure_total_missing_days_map_centroids_tile_",pred_mod_name,"_",
-                     "_",out_suffix,".png",sep=""),
-      width=col_mfrow*res_pix,height=row_mfrow*res_pix)
-  #spplot for reg_layer not working on NEX again
-  #p_shp <- spplot(reg_layer,"ISO3" ,col.regions=NA, col="black") #ok problem solved!!
-  p_r <-levelplot(r_mask,colorkey=F) #no key legend
-  p <- bubble(df_missing_tiles_sp,"tot_missing",main=paste0("Missing per tile and by ",pred_mod_name,
-                                                            " for ",y_var_name))
-  #p1 <- p+p_shp
-  p_c <- p + p_r + p #set the legend first by using p first
-  
-  try(print(p_c)) #error raised if number of missing values below a threshold does not exist
-  dev.off()
-  
-}else{
-  #do spplot after that on tot sum
-  res_pix <- 800
-  #res_pix <- 480
-  col_mfrow <- 1
-  row_mfrow <- 1
-  
-  png(filename=paste("Figure_total_predicted_days_predicted_map_centroids_tile_",pred_mod_name,"_",
-                     "_",out_suffix,".png",sep=""),
-      width=col_mfrow*res_pix,height=row_mfrow*res_pix)
-  
-  #bubble(pp, "att",
-  #       panel=function(...) {
-  #         sp.polygons(SpP, fill="blue")
-  #         sp:::panel.bubble(...)
-  #       }) 
-  
-  #p_shp <- spplot(reg_layer,"ISO3" ,sp.layout=list("sp.polygons", reg_layer, fill="white"))
-  #p_shp spplot not working on NEX anymore, use raster as background
-  p_r <-levelplot(r_mask,colorkey=F) #no key legend
-  p <- bubble(df_missing_tiles_sp,"tot_pred",main=paste0("Prediction per tile and by ",pred_mod_name,
-                                                         " for ", y_var_name))
-  p_c <- p + p_r + p #set the legend first by using p first
-  #p1 <- p+p_shp
-  try(print(p_c)) #error raised if number of missing values below a threshold does not exist
-  dev.off()
-}
 
 ## combine a make count+ plot
 
 
-
-
+############################# END OF SCRIPT ###################################
